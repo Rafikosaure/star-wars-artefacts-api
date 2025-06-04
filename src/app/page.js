33396b05b4f4d4d7532588f43f88c9d15cade6ee
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-06-04 16:33:32 +0200

    Peaufinage de la page de documentation de l'API avec modifications légères des textes et retravail sur le design responsive.

--- src/app/page.js
+++ src/app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
 
   const contributorPresentation01 = "Je suis Rafik Ben Sadi, développeur fullstack et fan d'univers de fantasy. Cette API disponible à tous est à l'origine prévue pour l'un de mes projets personnels, un Wiki sur Star Wars intégrant une boutique en ligne où l'on peut faire ses emplettes avec... des artefacts issus de Star Wars !"
 
-  const contributorPresentation02 = "Ces artefacts sont parfois imaginés ou inspirés de l'univers Canon, de Legends, ou bien sont totalement imaginés à partir du lore. On en trouve de tous les genres : les sabres laser blanc d'Ahsoka Tano, des ouvrages précieux tel le journal des Whills ou les livres sacrés des anciens Jedi... ou même un gigantesque fragment de l’Étoile de la Mort, pêché directement dans les profondeurs de Kef Bir !"
+  const contributorPresentation02 = "Ces artefacts sont parfois imaginés ou inspirés de l'univers Canon, de Legends, ou bien sont totalement imaginés à partir du lore. On en trouve de tous les genres : les sabres laser blanc d'Ahsoka Tano, des ouvrages précieux tel le journal intime de Padme Amidala ou les livres sacrés des anciens Jedi... ou même un gigantesque fragment de l’Étoile de la Mort, pêché directement dans les profondeurs de Kef Bir !"
 
   const JSONResponse = 
   `{
@@ -136,19 +136,29 @@ export default function Home() {
           <h3 className={styles.firstroute}><Image src={logoRebellion} alt="Logo de l'alliance rebelle" width={24} height={24} style={{ verticalAlign: 'middle' }} /> GET <span className={styles.code}>/api/products</span></h3>
           <p className={styles.quote}>Renvoie une liste de produits avec pagination, tri, recherche et filtres.</p>
           <ul className={styles.unorderedlist}>
-            <li className={styles.listitem}><code className={styles.code}>page</code> – numéro de page (par défaut : 1)</li>
-            <li className={styles.listitem}><code className={styles.code}>limit</code> – nombre d’éléments par page ou &quot;all&quot;</li>
-            <li className={styles.listitem}><code className={styles.code}>sort</code> – <code className={styles.code}>title_asc</code>, <code className={styles.code}>price_desc</code>, etc.</li>
-            <li className={styles.listitem}><code className={styles.code}>limitRandom</code> – à utiliser avec <code className={styles.code}>sort=random</code></li>
-            <li className={styles.listitem}><code className={styles.code}>tags</code>, <code className={styles.code}>excludeTags</code> – filtres par mots-clés</li>
-            <li className={styles.listitem}><code className={styles.code}>q</code> – recherche texte</li>
+            <li className={styles.listitem}><code className={styles.code}>page</code> – numéro de page (par défaut : 1), exemple : <code className={styles.code}>/api/products?page=10</code> vous donne accès à la page 10.</li>
+            <li className={styles.listitem}><code className={styles.code}>limit</code> – nombre d’éléments par page, exemple : <code className={styles.code}>/api/products?limit=20</code> vous donne accès à 20 éléments.<br />Astuce : <code className={styles.code}>/api/products?limit=all</code> vous donne accès à tous les éléments.</li>
+            <li className={styles.listitem}><code className={styles.code}>sort</code> – trier par :
+              <ul className={styles.unorderedlist}>
+                <li  className={styles.listitem}><code className={styles.code}>title_asc</code> - ordre alphabétique de titre, exemple : <code className={styles.code}>/api/products?sort=title_asc</code></li>
+                <li className={styles.listitem}><code className={styles.code}>title_desc</code> - ordre non-alphabétique de titre, exemple : <code className={styles.code}>/api/products?sort=title_desc</code></li>
+                <li className={styles.listitem}><code className={styles.code}>price_asc</code> - ordre croissant par prix, exemple : <code className={styles.code}>/api/products?sort=price_asc</code></li>
+                <li className={styles.listitem}><code className={styles.code}>price_desc</code> - ordre décroissant par prix, exemple : <code className={styles.code}>/api/products?sort=price_desc</code></li>
+                <li className={styles.listitem}><code className={styles.code}>random</code> - tri aléatoire, exemple : <code className={styles.code}>/api/products?sort=random</code></li>
+              </ul>
+            </li>
+            <li className={styles.listitem}><code className={styles.code}>limitRandom</code> – à utiliser avec <code className={styles.code}>sort=random</code>.<br/> Possiblité d&apos;ajouter <code className={styles.code}>limitRandom=N</code> pour limiter le nombre de résultats attendus.<br />Exemple pour 3 éléments choisis aléatoirement : <code className={styles.code}>/api/products?sort=random&limitRandom=3</code></li>
+            <li className={styles.listitem}><code className={styles.code}>tags</code>, <code className={styles.code}>excludeTags</code> – filtres par mots-clés.<br />Exemple pour récupérer les 10 premiers éléments comportant le tag &quot;jedi&quot; mais excluant le tag &quot;empire&quot; : <code className={styles.code}>/api/products?limit=10&tags=jedi&excludeTags=empire</code></li>
+            <li className={styles.listitem}><code className={styles.code}>q</code> – recherche textuelle. Les recherches textuelles concernent aussi bien les titres des éléments que leurs descriptions.<br />Exemple pour les 10 premiers éléments dont les titres et descriptions comportent le mot &quot;sabre&quot; : <code className={styles.code}>/api/products?limit=10&q=sabre</code></li>
           </ul>
 
           <h3 className={styles.route}><Image src={logoRebellion} alt="Logo de l'alliance rebelle" width={24} height={24} style={{ verticalAlign: 'middle' }} /> GET <span className={styles.code}>/api/products/[id]</span></h3>
-          <p className={styles.quote}>Renvoie un produit par son <code>id</code> exact.</p>
+          <p className={styles.quote}>Renvoie un produit par son <code className={styles.code}>id</code> exact.</p>
+          <p className={styles.quote}>Exemple pour récupérer l&apos;holocron de Dark Nihilus par son id : <code className={styles.code}>/api/products/67eebf482d4733d41bdfaccd</code></p>
 
           <h3 className={styles.route}><Image src={logoRebellion} alt="Logo de l'alliance rebelle" width={24} height={24} style={{ verticalAlign: 'middle' }} /> GET <span className={styles.code}>/api/products/by-title?title=...</span></h3>
           <p className={styles.quote}>Renvoie un produit à partir de son titre exact (recherche insensible aux majuscules et accents).</p>
+          <p className={styles.quote}>Exemple de syntaxe possible pour récupérer par son titre l&apos;holocron de Dark Nihilus : <code className={styles.code}>/api/products/by-title?title=L&apos;hoLôcron dé Dark nïHiluS</code></p>
 
           <h2 className={styles.subheading}><Image src={deathStar} alt="Etoile de la mort" width={32} height={32} style={{ verticalAlign: 'middle' }} /> Exemple de réponse</h2>
             <CodeBlock code={JSONResponse} />
