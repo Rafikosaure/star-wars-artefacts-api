60754b956de35e4a20705503532f5061f82ab8b7
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-06-15 18:16:25 +0200

    retour à la normale avec l'url d'image dans une variable dédiée

--- src/app/components/SEO.jsx
+++ src/app/components/SEO.jsx
@@ -32,7 +32,7 @@ export default function SEO() {
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
       <meta property="og:site_name" content={siteName} />
-      <meta property="og:image" content='https://star-wars-artefacts-api.vercel.app/images/documentation/seo_banner.jpg' />
+      <meta property="og:image" content={image} />
       <meta property="og:locale" content={locale} />
       <meta property="og:type" content="website" />
 
@@ -40,7 +40,7 @@ export default function SEO() {
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content='https://star-wars-artefacts-api.vercel.app/images/documentation/seo_banner.jpg' />
+      <meta name="twitter:image" content={image} />
 
       {/* JSON-LD */}
       <script
