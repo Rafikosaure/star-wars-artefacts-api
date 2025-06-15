cf6354f449ee78a40b988608cd2b521761e24e4b
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-06-15 19:06:29 +0200

    Amélioration du composant SEO.

--- src/app/components/SEO.jsx
+++ src/app/components/SEO.jsx
@@ -28,15 +28,18 @@ export default function SEO() {
       <meta name="description" content={description} />
 
       {/* Open Graph */}
+      <meta property="og:image" content={image} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:url" content={url} />
       <meta property="og:site_name" content={siteName} />
-      <meta property="og:image" content={image} />
       <meta property="og:locale" content={locale} />
       <meta property="og:type" content="website" />
 
       {/* Twitter Cards */}
+      <meta name="twitter:site" content="@BenSadiRaf27412" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
