c791e600174b7e557c4dc95289d2e308f20f2b74
RAFIK BEN_SADI <RAFIKBENSADI@live.fr>
2025-07-04 20:08:58 +0200

    Test pour le référencement sur les réseaux sociaux (essai 4)

--- src/app/page.js
+++ src/app/page.js
@@ -20,37 +20,12 @@ import {
   JSONObjectExample
 } from "@/data/homePageData";
 import { 
-  // metadata, 
+  metadata, 
   jsonLd 
 } from "@/app/SEO";
 
 // Metadata pour le référencement
-export const metadata = {
-    title: 'Star Wars Artefacts API',
-    description: 'Une API REST pour explorer des artefacts rares issus de l’univers de Star Wars.',
-    openGraph: {
-        title: 'Star Wars Artefacts API',
-        description: 'Explorez des artefacts issus du Canon, des Légendes ou de l’imaginaire autour de Star Wars.',
-        url: config.baseUrl,
-        siteName: 'Star Wars Artefacts API',
-        images: [
-        {
-            url: `${config.baseUrl}/images/documentation/banner.jpg`,
-            width: 1200,
-            height: 630,
-            alt: 'API artefacts Star Wars',
-        },
-        ],
-        locale: 'fr_FR',
-        type: 'website',
-    },
-    twitter: {
-        card: 'summary_large_image',
-        title: 'Star Wars Artefacts API',
-        description: 'Une API REST dédiée aux artefacts de l’univers Star Wars.',
-        images: [`${config.baseUrl}/images/documentation/banner.jpg`],
-    },
-};
+export { metadata };
 
 // Page de documentation de l'API
 export default function Home() {
